d53bdb439845532297ef4ef43a96aeedc9b01fb4
tiagoCMatias <tiago.matias92@gmail.com>
2018-10-17 09:53:59 +0100

    add subscriber writing to postgreSQL database

--- publisher/publisher.py
+++ publisher/publisher.py
@@ -1,20 +1,29 @@
 import paho.mqtt.client as mqtt
 import time
+import os
 
 # The callback for when the client receives a CONNACK response from the server.
+
+
 def on_connect(client, userdata, flags, rc):
     print("Connected with result code "+str(rc))
 
     print("publishing")
+    welcome_message = 0
     while True:
-        client.publish('mosquitto', 'Welcome to Mosq')
+        welcome_message = welcome_message+1
+        client.publish(os.environ['MOSQUITTO_TOPIC'],
+                       'Welcome to Mosq ', welcome_message)
         client.loop(.1)
+        if welcome_message > 1000
+        welcome_message = 0
 
 
 client = mqtt.Client()
 client.on_connect = on_connect
 
-client.connect("localhost", 1883, 60)
+client.connect(os.environ['MOSQUITTO_HOST'],
+               int(os.environ['MOSQUITTO_PORT']), 60)
 
 
 client.loop_forever()
--- subscriber/subscriber.py
+++ subscriber/subscriber.py
@@ -1,23 +1,69 @@
 import time
+import os
+import psycopg2 as psycopg2
 import paho.mqtt.client as mqtt
 
-broker="localhost"
+db_connection = None
+
+
+def connect_db():
+    try:
+        conn = psycopg2.connect(host="db",
+                                database=os.environ["DB_NAME"],
+                                user=os.environ["DB_USER"],
+                                password=os.environ["DB_PASS"])
+        conn.autocommit = True
+
+    except:
+        print("I am unable to connect to the database")
+        return None
+
+    cursor = conn.cursor()
+
+    # Create Log Table if not exist
+    query = """
+        CREATE TABLE IF NOT EXISTS mosquitto_log (
+            id SERIAL PRIMARY KEY,
+            topic VARCHAR(256),
+            message TEXT,
+            timestamp TIMESTAMP,
+        );
+    """
+
+    cursor.execute(query)
+
+    return cursor
+
+
+def save_to_db(topic, message):
+    query = """
+        INSERT INTO mosquitto_log (id, topic, message, timestamp) VALUES (DEFAULT, '{}', '{}', 'now()');
+    """.format(topic, str(message.decode()))
+    db_connection.execute(query)
+
 
 def on_message(client, userdata, message):
     time.sleep(1)
-    print("received message =",str(message.payload.decode("utf-8")))
+    print("received message =", str(message.payload.decode("utf-8")))
+    save_to_db(msg.topic, msg.payload)  # msg.qos
+
 
 def on_connect(client, userdata, flags, rc):
     print("Connected with result code "+str(rc))
-    client.subscribe('mosquitto')
+    client.subscribe(os.environ['MOSQUITTO_TOPIC'])
 
-client = mqtt.Client()
-client.on_connect = on_connect
-client.on_message=on_message
 
-client.connect(broker, 1883, 60)
+if __name__ == '__main__':
 
-print("connecting to broker ",broker)
+    db_connection = connect_db()
 
-client.loop_forever()
+    client = mqtt.Client()
+    client.on_connect = on_connect
+    client.on_message = on_message
 
+    client.connect(os.environ['MOSQUITTO_HOST'],
+                   int(os.environ['MOSQUITTO_PORT']), 60)
+
+    print("connecting to broker -", os.environ['MOSQUITTO_HOST'])
+
+    client.loop_forever()
